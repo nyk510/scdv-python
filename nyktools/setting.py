e4c6450b2bf77ed8b04e60519f521144ff8520ea
nyker510 <takamail53@gmail.com>
2019-02-23 23:36:50 +0900

    [update] change data dir by env value
    default is same

--- nyktools/setting.py
+++ nyktools/setting.py
@@ -4,7 +4,7 @@ LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-DATA_DIR = '/data'
+DATA_DIR = os.getenv('DATA_DIR', os.path.join(PROJECT_ROOT, 'data'))
 
 FEATURE_DIR = os.path.join(DATA_DIR, 'feature')
 os.makedirs(FEATURE_DIR, exist_ok=True)
@@ -12,7 +12,5 @@ os.makedirs(FEATURE_DIR, exist_ok=True)
 PROCESSED_ROOT = os.path.join(DATA_DIR, 'processed')
 os.makedirs(PROCESSED_ROOT, exist_ok=True)
 
-RAW_DATA_DIR = os.path.join(DATA_DIR, 'raw')
-
-PREDICT_FOR_TEST_DIR = os.path.join(DATA_DIR, 'predict_test')
-os.makedirs(PREDICT_FOR_TEST_DIR, exist_ok=True)
+DATASET_DIR = os.path.join(DATA_DIR, 'dataset')
+os.makedirs(DATASET_DIR, exist_ok=True)
